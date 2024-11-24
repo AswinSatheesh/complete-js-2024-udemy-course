075d9c40ace86bf390050276ac5102f9be552f6d
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2024-11-24 12:47:46 +0530

    assignment1 completed

---
+++ LECTURE-PRACTISE/assignments.js
@@ -0,0 +1,7 @@
+let country = "India";
+let continent = "Asia";
+let population = "3 Million"
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- LECTURE-PRACTISE/script.js
+++ LECTURE-PRACTISE/script.js
@@ -1,4 +1,11 @@
 let js = 'amazing';
-if (js === 'amazing') alert('Js is Fun');
+// if (js === 'amazing') alert('Js is Fun');
 
 console.log(60 - 50 + 1);
+
+let firstName = "Aswin";
+
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
