dd75e87bd0c33997e377c2d944b1bda61859feb2
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2024-12-14 08:39:50 +0530

    String and Template Literalas

--- LECTURE-PRACTISE/assignments.js
+++ LECTURE-PRACTISE/assignments.js
@@ -1,7 +1,7 @@
-let country = "India";
-let continent = "Asia";
-let population = "3 Million"
+// let country = "India";
+// let continent = "Asia";
+// let population = "3 Million"
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
--- LECTURE-PRACTISE/script.js
+++ LECTURE-PRACTISE/script.js
@@ -21,14 +21,37 @@ console.log(firstName);
 // console.log(typeof year)
 
 
-const currentYear = 2024;
-const aswinAge = 2024 - 2001;
-const kannanAge = 2024 - 2002;
+// const currentYear = 2024;
+// const aswinAge = 2024 - 2001;
+// const kannanAge = 2024 - 2002;
 
 
 
-console.log(aswinAge, kannanAge, 2 ** 3);
+// console.log(aswinAge, kannanAge, 2 ** 3);
+
+// const firstName = 'Aswin';
+// const lastName = 'Satheesh';
+// console.log(firstName + ' ' + lastName)
+
+// strings Template Literals
 
 const firstName = 'Aswin';
-const lastName = 'Satheesh';
-console.log(firstName + ' ' + lastName)
+const job = "Developer";
+const birthYear = 2001;
+const currentYear = 2024;
+
+const aswin = "i'm " + firstName + ', a ' + (currentYear - birthYear) + ' year old, ' + job + '.'
+
+console.log(aswin);
+
+//Maintaining space is a tough thing in string
+
+//MultiLine String
+const result = `I'm ${firstName}, a ${currentYear - birthYear} year old, ${job}!`;
+console.log(result);
+
+console.log("String with \n\ multiple \n\ lines");
+
+console.log(`String with
+    Multiple 
+    Lines`);
