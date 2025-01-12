915d524aa5d6b31f246bc2d825998085ceda7984
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2025-01-12 11:54:10 +0530

    strict mode and reserved mode

---
+++ Fundamentals-Part2/starter/script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+let ableToAttendInv = false;
+const passInv = true;
+
+if (passInv) ableToAttendInv = true;
+if (ableToAttendInv) console.log("Interview Cleared!!");
+
+// let private = 'Aswin'; reserved word
+// let interface = 5; reserved word
+// let if = true;
