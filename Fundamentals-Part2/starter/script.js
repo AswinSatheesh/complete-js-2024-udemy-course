f36f7be7ac840e8859f79d6288f7b3957695e7d7
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2025-01-12 15:38:34 +0530

    function in js

--- Fundamentals-Part2/starter/script.js
+++ Fundamentals-Part2/starter/script.js
@@ -1,11 +1,32 @@
 'use strict'
 
-let ableToAttendInv = false;
-const passInv = true;
+// let ableToAttendInv = false;
+// const passInv = true;
 
-if (passInv) ableToAttendInv = true;
-if (ableToAttendInv) console.log("Interview Cleared!!");
+// if (passInv) ableToAttendInv = true;
+// if (ableToAttendInv) console.log("Interview Cleared!!");
 
 // let private = 'Aswin'; reserved word
 // let interface = 5; reserved word
 // let if = true;
+
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} Apples and ${oranges} Oranges`;
+//     return juice;
+// }
+
+
+// const processor1 = fruitProcessor(5, 0)
+// console.log(processor1);
+// const processor2 = fruitProcessor(2, 4);
+// console.log(processor2);
+
+
+const age = function (birthYear) {
+    const currentAge = 2025 - birthYear;
+    return currentAge;
+}
+
+const myage = age(2001);
+console.log(myage);
