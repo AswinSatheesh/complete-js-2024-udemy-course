0570f4c3c2b02d1ec35d8835a0c23a94df2045b3
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2025-01-20 21:53:46 +0530

    live server installed

--- Developer Skills/starter/script.js
+++ Developer Skills/starter/script.js
@@ -1,4 +1,4 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-console.log('Npm live server installed');
+console.log(`Npm live server installed use command 'live-server'`);
