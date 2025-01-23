f2b6c4c265eacd2f14581c206fde1002313d46d0
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2025-01-23 20:41:11 +0530

    challenge 1 step by step solving

--- Developer Skills/starter/script.js
+++ Developer Skills/starter/script.js
@@ -71,4 +71,10 @@
 // TEST DATA 1 : [17,21,23]
 // TEST DATA 2 : [12,5,-5,0,4]
 
-const printForecast = function (arr) {};
+const printForecast = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(` ...${arr[i]}^C in ${i + 1} days `);
+  }
+};
+
+printForecast([17, 21, 23]);
