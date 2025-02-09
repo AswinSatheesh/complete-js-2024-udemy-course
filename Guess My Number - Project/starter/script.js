34f51242a3557bb642c186cc490b3709f4b27c18
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2025-02-09 10:23:17 +0530

    dom manipulation

--- Guess My Number - Project/starter/script.js
+++ Guess My Number - Project/starter/script.js
@@ -3,3 +3,12 @@
 console.log(document.querySelector('.message').textContent);
 
 // console.log(document.querySelector('.between').textContent);
+document.querySelector('.message').textContent = '🎉Correct Number...';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 10;
+document.querySelector('.score').textContent = 15;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 10;
