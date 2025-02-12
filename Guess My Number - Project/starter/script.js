3f20adeb3e6a7ecae74805b76ff4eac70f08f031
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2025-02-12 23:00:49 +0530

    game finished and working fine

--- Guess My Number - Project/starter/script.js
+++ Guess My Number - Project/starter/script.js
@@ -19,6 +19,7 @@ document.querySelector('.guess').value = 10;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.score').textContent = score;
 
@@ -33,6 +34,11 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '🎉Correct Number';
       document.querySelector('body').style.backgroundColor = '#60b347';
       document.querySelector('.number').style.width = '30rem';
+
+      if (score > highScore) {
+        highScore = score;
+        document.querySelector('.highscore').textContent = highScore;
+      }
     }
   } else if (inputNumber < secretNumber) {
     if (score > 1) {
@@ -74,3 +80,5 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
+
+//Implementing HighScore Feature
