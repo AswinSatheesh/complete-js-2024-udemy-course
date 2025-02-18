d0cfde26cd7f29f82f43d08c45b7aad4aacc2615
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2025-02-18 21:58:54 +0530

    button click

--- Pig-Game/starter/script.js
+++ Pig-Game/starter/script.js
@@ -4,7 +4,16 @@
 const score0Ele = document.querySelector('#score--0');
 const score1Ele = document.getElementById('score--1'); //more speed when i comes to accessing Id
 const diceEle = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
 //Starting condition
 score0Ele.textContent = 0;
 score1Ele.textContent = 0;
 diceEle.classList.add('hidden');
+
+//Dice Roll Logic while click button
+
+btnRoll.addEventListener('click', function () {
+  console.log('Button Roll Clicked');
+});
