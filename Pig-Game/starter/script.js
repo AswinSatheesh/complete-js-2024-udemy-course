bc4f4c729d95572c2832844da8c17d6c7adcbe4f
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2025-02-18 08:00:08 +0530

    Dice startig conditions added

--- Pig-Game/starter/script.js
+++ Pig-Game/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+//Selecting both score dom elements
+const score0Ele = document.querySelector('#score--0');
+const score1Ele = document.getElementById('score--1'); //more speed when i comes to accessing Id
+const diceEle = document.querySelector('.dice');
+//Starting condition
+score0Ele.textContent = 0;
+score1Ele.textContent = 0;
+diceEle.classList.add('hidden');
