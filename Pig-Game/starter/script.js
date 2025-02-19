d1113345bcec135056b70e497cdf85cfcc9e87f4
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2025-02-19 22:30:43 +0530

    current score and toggle concept added

--- Pig-Game/starter/script.js
+++ Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //Selecting both score dom elements
+const playerEl0 = document.querySelector('.player--0');
+const playerEl1 = document.querySelector('.player--1');
 const score0Ele = document.querySelector('#score--0');
 const score1Ele = document.getElementById('score--1');
 const current0Ele = document.getElementById('current--0');
@@ -15,7 +17,9 @@ score0Ele.textContent = 0;
 score1Ele.textContent = 0;
 diceEle.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 //Dice Roll Logic while click button
 
 btnRoll.addEventListener('click', function () {
@@ -32,8 +36,23 @@ btnRoll.addEventListener('click', function () {
 
   if (randomRoll != 1) {
     currentScore += randomRoll;
-    current0Ele.textContent = currentScore;
+    // current0Ele.textContent = currentScore; //This one always sets the first element scroe //dynamic value set
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //check the rolled =1, then switch the player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    // if (activePlayer === 0) {
+    //   playerEl0.classList.add('player--active');
+    //   playerEl1.classList.remove('player--active');
+    // } else {
+    //   playerEl0.classList.remove('player--active');
+    //   playerEl1.classList.add('player--active');
+    // } //instead of this if condition we can use toggle consept in classList
+
+    playerEl0.classList.toggle('player--active');
+    playerEl1.classList.toggle('player--active');
   }
 });
