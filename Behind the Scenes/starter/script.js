153ee87aa4e5074633a2cb53896b061c385b1029
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2025-03-02 21:11:15 +0530

    implementation

--- Behind the Scenes/starter/script.js
+++ Behind the Scenes/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2025 - birthYear;
+  console.log(firstName);
+  function printAge() {
+    const output = `Your are ${age} old and born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Aswin';
+calcAge(2001);
