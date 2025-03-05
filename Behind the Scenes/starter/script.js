2b24f08409c219d80ef2ddd025244013f6a0a545
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2025-03-05 22:01:14 +0530

    TDZ

--- Behind the Scenes/starter/script.js
+++ Behind the Scenes/starter/script.js
@@ -1,15 +1,25 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2025 - birthYear;
-  console.log(firstName);
-  function printAge() {
-    const output = `Your are ${age} old and born in ${birthYear}`;
-    console.log(output);
-  }
-  printAge();
-  return age;
-}
+// function calcAge(birthYear) {
+//   const age = 2025 - birthYear;
+//   console.log(firstName);
+//   function printAge() {
+//     const output = `Your are ${age} old and born in ${birthYear}`;
+//     console.log(output);
+//   }
+//   printAge();
+//   return age;
+// }
 
-const firstName = 'Aswin';
-calcAge(2001);
+// const firstName = 'Aswin';
+// calcAge(2001);
+
+// Temporal Dead Zone
+const myName = 'Aswin';
+
+if (myName === 'Aswin') {
+  console.log(`job is ${job}`); //TDZ
+  const exp = 5; //TDZ
+  const job = 'Engineer';
+  console.log(job);
+}
