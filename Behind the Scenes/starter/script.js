b3aaebe62fd499fc9d5b6e26b16ebcebf0a6b470
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2025-03-10 23:03:53 +0530

    hoisting example

--- Behind the Scenes/starter/script.js
+++ Behind the Scenes/starter/script.js
@@ -15,11 +15,45 @@
 // calcAge(2001);
 
 // Temporal Dead Zone
-const myName = 'Aswin';
+// const myName = 'Aswin';
 
-if (myName === 'Aswin') {
-  console.log(`job is ${job}`); //TDZ
-  const exp = 5; //TDZ
-  const job = 'Engineer';
-  console.log(job);
+// if (myName === 'Aswin') {
+//   // console.log(`job is ${job}`); //TDZ
+//   const exp = 5; //TDZ
+//   const job = 'Engineer';
+//   console.log(job);
+// }
+
+// console.log(a);
+// a = 10;
+// console.log(a);
+
+// let a;
+
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+// var me = 'Aswin';
+// let job = 'Programmer';
+// const year = 2025;
+
+//Functions
+
+console.log(addTwo(5, 4));
+// console.log(TwoAdd(5, 5));
+// console.log(TwoAdd); //var TwoAdd itself is a undefined
+console.log(arr(5, 1));
+
+function addTwo(a, b) {
+  return a + b;
 }
+
+var TwoAdd = function (a, b) {
+  //variable declared using var is hoisting and value will we undefined now we are trying to call function with undefined. //undefined(5,5)
+  return a + b;
+};
+
+const arr = (a, b) => {
+  return a + b;
+};
